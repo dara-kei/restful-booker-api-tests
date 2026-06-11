9c346c2f634f48e2fa0bbad823cf632984d9986d
Daria <darya.keido@gmail.com>
2026-06-11 10:25:58 +0200

    Added new api tests to test_update_booking.py

--- tests/booking/test_update_booking.py
+++ tests/booking/test_update_booking.py
@@ -1,11 +1,14 @@
+from wsgiref import headers
+
 from schemas.schema_get_one_booking import get_one_booking_schema
 from jsonschema import validate
+import pytest
 
 
 ENDPOINT = 'booking'
 
 
-def test_update_booking(api_client, create_booking, get_token):
+def test_update_booking(api_client, create_booking, token):
     payload = {
     "firstname" : "James",
     "lastname" : "Brown",
@@ -17,7 +20,7 @@ def test_update_booking(api_client, create_booking, get_token):
     },
     "additionalneeds" : "Breakfast"
 }
-    headers = {'Cookie' : f'token={get_token}'}
+    headers = {'Cookie' : f'token={token}'}
     response = api_client.put(f'{ENDPOINT}/{create_booking['bookingid']}', json=payload, headers=headers)
     assert response.status_code == 200
     actual_response = response.json()
@@ -25,7 +28,7 @@ def test_update_booking(api_client, create_booking, get_token):
     assert actual_response['firstname'] == 'James'
 
 
-def test_update_booking_with_invalid_data(api_client, create_booking, get_token):
+def test_update_booking_without_last_name(api_client, create_booking, token):
     payload = {
     "firstname" : "James",
     "totalprice" : 111,
@@ -36,7 +39,7 @@ def test_update_booking_with_invalid_data(api_client, create_booking, get_token)
     },
     "additionalneeds" : "Breakfast"
 }
-    headers = {'Cookie' : f'token={get_token}'}
+    headers = {'Cookie' : f'token={token}'}
     response = api_client.put(f'{ENDPOINT}/{create_booking['bookingid']}', json=payload, headers=headers)
     assert response.status_code == 400
 
@@ -54,3 +57,82 @@ def test_update_booking_without_token(api_client, create_booking):
 }
     response = api_client.put(f'{ENDPOINT}/{create_booking['bookingid']}', json=payload)
     assert response.status_code == 403
+
+
+def test_update_not_existing_booking(api_client, token):
+    payload = {
+        "firstname": "James",
+        "lastname": "Brown",
+        "totalprice": 111,
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2018-01-01",
+            "checkout": "2019-01-01"
+        },
+        "additionalneeds": "Breakfast"
+    }
+    headers = {'Cookie': f'token={token}'}
+    response = api_client.put(f'{ENDPOINT}/99999999', json=payload, headers=headers)
+    assert response.status_code == 405
+
+
+@pytest.mark.xfail(
+    reason="API-XX: PUT /booking incorrectly processes invalid booking ids"
+)
+@pytest.mark.parametrize("booking_id", ["1abc", "1.1", "01"])
+def test_update_booking_with_invalid_id(api_client, token, booking_id):
+    payload = {
+        "firstname": "James",
+        "lastname": "Brown",
+        "totalprice": 111,
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2018-01-01",
+            "checkout": "2019-01-01"
+        },
+        "additionalneeds": "Breakfast"
+    }
+    headers = {'Cookie': f'token={token}'}
+    response = api_client.put(f'{ENDPOINT}/{booking_id}', json=payload, headers=headers)
+    assert response.status_code == 404
+
+
+@pytest.mark.xfail(
+    reason="Known bug: API accepts negative total price during booking update"
+)
+def test_update_booking_with_negative_price(api_client,create_booking, token):
+    payload = {
+        "firstname": "James",
+        "lastname": "Brown",
+        "totalprice": -111,
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2018-01-01",
+            "checkout": "2019-01-01"
+        },
+        "additionalneeds": "Breakfast"
+    }
+    headers = {'Cookie': f'token={token}'}
+    response = api_client.put(f'{ENDPOINT}/{create_booking["bookingid"]}', json=payload, headers=headers)
+    assert response.status_code == 400
+
+
+@pytest.mark.xfail(
+    reason="Known bug: API accepts checkout date earlier than checkin date during booking update"
+)
+def test_update_booking_invalid_date(api_client,create_booking, token):
+    payload = {
+        "firstname": "James",
+        "lastname": "Brown",
+        "totalprice": 111,
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2020-01-01",
+            "checkout": "2019-01-01"
+        },
+        "additionalneeds": "Breakfast"
+    }
+    headers = {'Cookie': f'token={token}'}
+    response = api_client.put(f'{ENDPOINT}/{create_booking["bookingid"]}', json=payload, headers=headers)
+    assert response.status_code == 400
+    
